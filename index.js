f1f9e379abdd540d6ee94bd53ebe75fe143678f8
ammangame748-dev <ammangame748@gmail.com>
2026-08-01 06:06:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '')));
+// تشغيل الملفات الثابتة من المجلد الحالي
+app.use(express.static(__dirname));
 
+// أي طلب يجي للموقع يرجع ملف index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'), (err) => {
+        if (err) {
+            res.status(404).send("خطأ: ملف index.html غير موجود في المسار الرئيسي للسيرفر!");
+        }
+    });
 });
 
 app.listen(PORT, () => {
